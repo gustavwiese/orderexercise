da83cebfbef076db009ebc6d1038b131e69aa8be
gustavwiese <gusse12345@gmail.com>
2024-02-16 09:57:40 +0100

    første commit

---
+++ src/main/java/kea/exercise/orderexercise/OrderexerciseApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.orderexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderexerciseApplication.class, args);
+    }
+
+}
